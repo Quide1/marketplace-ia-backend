9fcbd044a40b4848e25996a2f151d83c98723182
Quide1 <ezequielr.dev@gmail.com>
2024-09-12 01:33:58 -0300

    add modularization

--- src/lib/puppeteer.ts
+++ src/lib/puppeteer.ts
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 import { closeModal } from "./closeModal";
 import { scrollPage } from "./scroll";
+import { getDataOfPageLink } from "./getDataOfPageLink";
 
 export const puppeteerHandler = async () => {
   console.log("inicializando puppeteer");
@@ -8,21 +9,16 @@ export const puppeteerHandler = async () => {
     const browser = await puppeteer.launch({ headless: false, slowMo: 1000 });
     const page = await browser.newPage();
     await page.goto(
-      "https://www.facebook.com/marketplace/106423786059675/search?minPrice=2000000&maxPrice=3000000&query=kawasaki&exact=false"
+      "https://www.facebook.com/marketplace/item/1224815315224535/?ref=search&referral_code=null&referral_story_type=post"
     );
     await closeModal(page);
-    await scrollPage(page)
-    const arrayLinks = await page.evaluate(() => {
-      const nodesMarketPlace = (globalThis as any).document.querySelectorAll(
-        'a[href^="/marketplace/item/"]'
-      );
-      const data = [...nodesMarketPlace].map((node) => {
-        return node.href;
-      });
-      return data
-    });
-    console.log(arrayLinks)
-    await page.screenshot({ path: "example.png" });
+   
+    const link ="https://www.facebook.com/marketplace/item/1224815315224535/?ref=search&referral_code=null&referral_story_type=post"
+    console.log(link)
+    const dataPage = await getDataOfPageLink(
+      page,
+    );
+    console.log(dataPage);
   } catch (error) {
     console.log(error);
   }
@@ -30,16 +26,3 @@ export const puppeteerHandler = async () => {
 
 puppeteerHandler();
 
-/**
- * 
- * // Selecciona todos los elementos <a> cuyo href comience con "/marketplace/item/"
-const anchors = document.querySelectorAll('a[href^="/marketplace/item/"]');
-
-// Itera sobre los nodos y accede al atributo href
-anchors.forEach(anchor => {
-    console.log(anchor.href); // Muestra el valor del href de cada <a>
-});
-
- * 
- * 
- */
