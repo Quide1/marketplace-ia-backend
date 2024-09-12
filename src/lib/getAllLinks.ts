6865b6194791ecaaa0c4e7f64b8ba9d4524809a4
Quide1 <ezequielr.dev@gmail.com>
2024-09-12 01:33:30 -0300

    Add a get all links functionality

--- src/lib/getAllLinks.ts
+++ src/lib/getAllLinks.ts
@@ -1,5 +1,15 @@
 import { Page } from "puppeteer";
-
+import { scrollPage } from "./scroll";
 export const getAllLinks = async(page:Page)=>{
-    await page.waitForSelector
+   await scrollPage(page)
+    const arrayLinks = await page.evaluate(() => {
+      const nodesMarketPlace = (globalThis as any).document.querySelectorAll(
+        'a[href^="/marketplace/item/"]'
+      );
+      const data = [...nodesMarketPlace].map((node) => {
+        return node.href;
+      });
+      return data
+    });
+    return arrayLinks
 }
