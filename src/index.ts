775203b4254bd8adca9076ae2ece16eeb25b3f35
Quide1 <ezequielr.dev@gmail.com>
2024-09-17 00:27:47 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use("/scrapper", scrapperRouter);
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
