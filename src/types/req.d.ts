4f0b0f23036a822854a01b14cfad97fee222856f
Quide1 <ezequielr.dev@gmail.com>
2024-09-25 15:02:28 -0300

    Create

--- src/types/req.d.ts
+++ src/types/req.d.ts
@@ -1,9 +1,7 @@
 import { type PublicationData } from "./publicationData"
+import { GenerateTextParams } from "./geminiLlm"
 export interface RequestParam {
     link:string
     timeScroll:number
 }
-
-export interface RequestBody {
-    body:PublicationData[]
-}
+export interface RequestBody extends GenerateTextParams {}
